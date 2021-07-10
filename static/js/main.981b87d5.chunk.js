864e509a58f33cdb1046f7b50293cddeb1558c9d
ASUS <abdalrahmandaas963@gmail.com.com>
2021-07-10 18:56:19 +0300

    Updates

--- static/js/main.981b87d5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfull=this.webpackJsonpfull||[]).push([[0],{11:function(n,t,e){"use strict";e.r(t);var c=e(1),i=e.n(c),o=e(3),r=e.n(o),u=(e(8),e.p,e(9),e(0));var s=function(n){n&&n instanceof Function&&e.e(3).then(e.bind(null,12)).then((function(t){var e=t.getCLS,c=t.getFID,i=t.getFCP,o=t.getLCP,r=t.getTTFB;e(n),c(n),i(n),o(n),r(n)}))};r.a.render(Object(u.jsx)(i.a.StrictMode,{children:Object(u.jsx)("h1",{children:"ready"})}),document.getElementById("root")),s()},8:function(n,t,e){},9:function(n,t,e){}},[[11,1,2]]]);
-//# sourceMappingURL=main.981b87d5.chunk.js.map
